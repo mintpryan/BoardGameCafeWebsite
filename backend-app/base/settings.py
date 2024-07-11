65b8a1984a0d97a4437cf9cfa3b1e8887692dcde
Iana <ymarkova@appline.ru>
2024-07-11 14:22:05 +0500

    change static_root

--- backend-app/base/settings.py
+++ backend-app/base/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MODEL_ROOT = '3dModels/'
 
