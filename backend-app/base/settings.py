e0593df158c0435e77aec26581628ddc4587a006
Iana <ymarkova@appline.ru>
2024-07-11 14:06:33 +0500

    add allowed host for digital ocean

--- backend-app/base/settings.py
+++ backend-app/base/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-6h4@1qeutle9v7b-3^h$3&d@2ji0zq&lmg=^#^nq=&2_b#egm7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['161.35.1.134','localhost']
+ALLOWED_HOSTS = ['clownfish-app-ecte4.ondigitalocean.app','localhost']
 
 
 # Application definition
