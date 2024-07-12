52c7616fa7dd1fb25e75b5c7380897165e9d27d5
Iana <ymarkova@appline.ru>
2024-07-12 18:14:46 +0500

    final changes

--- backend-app/base/settings.py
+++ backend-app/base/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
