a121d6f75553b8dd14c4f24b3a88c86a9890bbca
Iana <ymarkova@appline.ru>
2024-07-12 16:19:03 +0500

    add auto creation user

---
+++ backend-app/manage/create_superuser.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+    help = 'Create a superuser'
+
+    def handle(self, *args, **kwargs):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@example.com',
+                password='adminpassword'
+            )
+            self.stdout.write(self.style.SUCCESS('Successfully created new superuser'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
